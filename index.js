33853be266e067e9ccd13d7773404e06030ea0f4
Akeem <AkeemS11788@gmail.com>
2021-11-03 08:28:39 -0400

    commit tree aab0fd66dcc28d1e909cf02e2e657f6ccc44f189

---
+++ index.js
@@ -0,0 +1,11 @@
+// const divide = function(){};
+
+const divide = (num1, num2) => 2000 / 100;
+
+// const square = function(){};
+
+const square = num3 => num3 ** 2;
+
+// const add = function(){};
+
+const add = (a, b) => a + b;
